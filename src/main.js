87e755834d4095e40b2f9e38b95688d50c731d67
GabrielMaiaBR <fgabrielsmaiaf@gmail.com>
2023-10-02 15:08:33 -0300

    a

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "@/router";
+
+import vue3GoogleLogin from "vue3-google-login";
+
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+app.use(vue3GoogleLogin, {
+  clientId: "977328464498-g1mmbuipb57d8pufbhp5uf4v76ap19up.apps.googleusercontent.com",
+});
+
+app.mount("#app");
